ad5816946e9515989d53d58ba2452315a25820f7
AlanKolmakov <kolm.a.s.1983@gmail.com>
2021-09-28 00:18:57 +0800

    Task 9. Log value

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-'use strict'
+'use strict';
+
+/**
+ * @param {Object} course
+ * @param {number} [course.id]
+ * @param {string} [course.name]
+ * @param {number} [course.year]
+ */
+const logValues = course => {
+    console.log(course.id, course.name, course.year);
+};
+
+// Sample usage - do not modify
+logValues({ id: 1, name: "Learn JavaScript", year: 2021 });
+// should log (separately): 1, "Learn JavaScript", 2021
